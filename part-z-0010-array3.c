eba4a91425293b9c0bb5e1b7a1f19bb07150840c
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-02-01 22:46:39 +0300

    Create part-z-0010-array3.c

---
+++ part-z-0010-array3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+    int ages[4];
+
+    ages[0] = 17;
+    ages[1] = 27;
+    ages[2] = 37;
+    ages[3] = 47;
+
+    // ages[75] = 77775;
+
+    // printf("What! %d", ages[75]); // What! 77775
+
+    return 0;
+}
+
